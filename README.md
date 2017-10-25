07cbbf8c78d4776fe956932e7320dc98cd7bd080
tt <953632795@qq.com>
2017-10-25 19:17:02 +0800

    add file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# grunt
+# lesson-1
+
+###  sudo npm install -g grunt-cli           安装全局grunt
+###  cd 文件夹
+###  npm init --yes
+###  cnpm install grunt --save
+###  grunt
+###  cnpm install grunt-contrib-cssmin --save           css压缩
+###  cnpm install grunt-contrib-uglify --save           js压缩
---
+++ gruntapp/Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt){
+	//配置grunt方法
+	grunt.initConfig({
+		pkg:grunt.file.readJSON('package.json'),
+		cssmin:{
+			combine:{
+				files:{
+					"app/css/style.css":["app/css/background.css","app/css/fontsize.css"]
+				}
+			}
+		},
+		uglify:{
+			dist:{
+				files:{
+					"app/js/main.min.js":["app/js/main.js"]
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+
+	grunt.registerTask("default",['cssmin','uglify']);
+
+	// grunt.registerTask("default",'',function(){
+	// 	grunt.log.write('在终端运行grunt命令,就会执行这句话!');
+	// });
+};
